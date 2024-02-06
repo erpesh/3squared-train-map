41c9e89486e74a61bc8e4a08bbd3ead254da28b2
Anton <antonio.fiasco24@gmail.com>
2024-02-06 13:14:49 +0000

    updated query

--- src/TrainMap.js
+++ src/TrainMap.js
@@ -32,7 +32,16 @@ const MyMap = ({ onTrainSelect }) => {
 
     const fetchTrainData = async () => {
         try {
-            const trainResponse = await apiRequest('/trains/tiploc/LEEDS,NEWHVTJ,CAMBDGE,CREWEMD,GTWK,WLSDEUT,HLWY236,LOWFRMT,WLSDRMT,LINCLNC,GLGC,CARLILE,MOSEUPY,KNGX,STAFFRD/2024-01-30T00:00:00/2024-01-30T23:59:59');
+            let today = new Date();
+
+            let year = today.getFullYear();
+            let month = String(today.getMonth() + 1).padStart(2, '0');
+            let day = String(today.getDate()).padStart(2, '0');
+
+            let startDate = `${year}-${month}-${day}T00:00:00`;
+            let endDate = `${year}-${month}-${day}T23:59:59`;
+
+            const trainResponse = await apiRequest(`/trains/tiploc/LEEDS,NEWHVTJ,CAMBDGE,CREWEMD,GTWK,WLSDEUT,HLWY236,LOWFRMT,WLSDRMT,LINCLNC,GLGC,CARLILE,MOSEUPY,KNGX,STAFFRD/${startDate}/${endDate}`);
             const trainData = await trainResponse.json();
             const filteredTrainData = trainData
                 .filter(item => item["lastReportedType"] === "DEPARTURE" ||
