2eed7a93a1cb92674f3270e1f0aa10311bb5fb39
Anton <antonio.fiasco24@gmail.com>
2024-02-19 12:49:12 +0000

    fixed gaps in train routes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ const App = () => {
     const [trains, setTrains] = useState([]);
     const [trainsWithMovement, setTrainsWithMovement] = useState([]);
 
-    async function fetchTrains() {
-        let today = new Date();
+    async function fetchTrains(date= null) {
+
+
+        let today = date ? new Date(date) : new Date();
 
         let year = today.getFullYear();
         let month = String(today.getMonth() + 1).padStart(2, '0');
@@ -22,7 +24,7 @@ const App = () => {
         let startDate = `${year}-${month}-${day}T00:00:00`;
         let endDate = `${year}-${month}-${day}T23:59:59`;
 
-        const trainResponse = await apiRequest(`/trains/tiploc/LEEDS,BELFASTC,JAMESST,NEWHVTJ,CAMBDGE,CREWEMD,GTWK,WLSDEUT,HLWY236,LOWFRMT,WLSDRMT,LINCLNC,GLGC,CARLILE,MOSEUPY,KNGX,STAFFRD/${startDate}/${endDate}`);
+        const trainResponse = await apiRequest(`/trains/tiploc/LEEDS,BELFASTC,JAMESST,MNCRPES,SHEFSRF,NEWHVTJ,CAMBDGE,CREWEMD,GTWK,WLSDEUT,HLWY236,LOWFRMT,WLSDRMT,LINCLNC,GLGC,CARLILE,MOSEUPY,KNGX,STAFFRD/${startDate}/${endDate}`);
         const trainData = await trainResponse.json();
         const filteredTrainData = trainData
             .filter(item => item["lastReportedType"] === "DEPARTURE" ||
@@ -36,8 +38,6 @@ const App = () => {
         setTrains(filteredTrainData);
         const trainsWithMovement = await getTrainsWithMovement(filteredTrainData);
         setTrainsWithMovement(trainsWithMovement);
-        console.log(trainsWithMovement);
-        // setSelectedTrain(filteredTrainData[0]);
     }
 
     async function getTrainsWithMovement(trains) {
