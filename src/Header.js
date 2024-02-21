2704199374a011f025d5612ab72ac84997b18afa
ramikota <158177554+ramikota@users.noreply.github.com>
2024-02-21 13:30:10 +0000

    edits

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <nav className="header">
       <img src="https://workable-application-form.s3.amazonaws.com/advanced/production/6246349ef4eee0e673b5e81a/02f6a105-676a-9b3a-a58b-58fc717de521" alt="Logo" className="logo" />
-      <h1 className="title">National Train Map</h1>
+      <h1 className="title">National Rail Map</h1>
     </nav>
   );
 };
