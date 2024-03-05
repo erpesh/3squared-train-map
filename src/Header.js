6dffc41b688a1deaac3d201f2fd95d8a323d9da0
Anton <antonio.fiasco24@gmail.com>
2024-03-05 14:53:05 +0000

    fix: mobile drawer button

--- src/Header.js
+++ src/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
                 src="https://workable-application-form.s3.amazonaws.com/advanced/production/6246349ef4eee0e673b5e81a/02f6a105-676a-9b3a-a58b-58fc717de521"
                 alt="Logo" className="logo"/>
             <h1 className="title">National Rail Map</h1>
-            <div>
+            <div className={'mobile-only'}>
                 <button onClick={toggleDrawer} className={'clean-button'}>
                     <AlignJustify/>
                 </button>
