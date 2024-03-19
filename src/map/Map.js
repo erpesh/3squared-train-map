73d594aa7ae24d12f7ba284b75d2b49ba27f6527
SultanFK1 <hasanfkusar@gmail.com>
2024-03-19 11:45:29 +0000

    Added ID's to each component so it can be used for unit testing

--- src/map/Map.js
+++ src/map/Map.js
@@ -31,26 +31,29 @@ const Map = ({ trains, selectedTrain, setSelectedTrain }) => {
         setRouteLine(selectedTrain.routes);
         setStations(selectedTrain.stations);
     };
-
+    
     // if (!trains || trains.length === 0) return <div>Error</div>
     if (!trains || trains.length === 0) return <div>Loading</div>
 
     return (
         <MapContainer
+            id = "map"
             zoomControl={false}
             center={[54, -0.5]}
             zoom={6}
             style={{ height: '100vh', background: 'ghostwhite' }}
         >
             <TileLayer
+    
                 url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
                 maxZoom={20}
                 attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+        
             />
 
-            {stations && <Stations stations={stations}/>}
-            {trains && trains.length > 0 && <Trains trains={trains} selectedTrain={selectedTrain} setSelectedTrain={setSelectedTrain}/>}
-            {routeLine && <Routes routeLine={routeLine}/>}
+            {stations && <Stations id = "map_stations" stations={stations}/>}
+            {trains && trains.length > 0 && <Trains id = "map_trains" trains={trains} selectedTrain={selectedTrain} setSelectedTrain={setSelectedTrain}/>}
+            {routeLine && <Routes id = "map_routes" routeLine={routeLine}/>}
             <ZoomControl position={'bottomleft'}/>
             <ScaleControl position={'bottomright'}/>
         </MapContainer>
